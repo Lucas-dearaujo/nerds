0e422aeee396ab5c7bc9d3559ef789cedbba6330
Lucas de Araujo <lucas.dearaujo.fmaia@gmail.com>
2026-01-06 09:01:02 -0300

    arrumando a main

--- src/main/java/com/ufc/nerds/NerdsApplication.java
+++ src/main/java/com/ufc/nerds/NerdsApplication.java
@@ -1,25 +1,13 @@
 package com.ufc.nerds;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.ufc.nerds.controller.PatrimonioController;
-import com.ufc.nerds.repositorio.PatrimonioRepositorio;
-import com.ufc.nerds.service.PatrimonioService;
 
 @SpringBootApplication
 public class NerdsApplication {
 
 	public static void main(String[] args) {
-		PatrimonioRepositorio estoque = new PatrimonioRepositorio();
-		PatrimonioService servicos = new PatrimonioService(estoque);
-		PatrimonioController head  = new PatrimonioController(servicos);
-		/* 
-		Patrimonio cadeira = new Patrimonio("cadeira" , "mobilia", "330454", true, true);
-		Patrimonio mesa = new Patrimonio("Mesa L", "Mobilia" , "226471" ,true, true );
-		Patrimonio notebook = new Patrimonio("asus" , "tecnologia", "61234" , true, true);
-		*/
-		//System.out.println(cadeira);
-		//SpringApplication.run(NerdsApplication.class, args);
+		SpringApplication.run(NerdsApplication.class, args);
 	}
 
 }
